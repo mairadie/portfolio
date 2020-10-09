39b3cf0b6073cf3b49a468b4cb2bdcd046b9fe2d
mairadie <pmtradie@gmail.com>
2020-10-09 10:26:42 +0800

    add initial draft

---
+++ server.py
@@ -0,0 +1,54 @@
+# set FLASK_ENV=development
+
+from flask import Flask, render_template, request, redirect
+import csv
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/<string:page_name>')
+def index(page_name):
+	return render_template(page_name)
+
+
+@app.route('/submit_form', methods=['POST', 'GET'])
+def submit_form():
+	if request.method == 'POST':
+		try:
+			data = request.form.to_dict()
+			write_to_csv(data)
+			return redirect('/thankyou.html')
+		except:
+			return 'did not save to database'
+	else:
+		return 'something went wrong yo'
+
+def write_to_file(data):
+	with open('database.txt', mode='a') as database:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+
+		file = database.write(f'\n{email},{subject},{message}')
+
+def write_to_csv(data):
+	with open('database.csv', mode='a', newline='') as database_csv:
+		email = data["email"]
+		subject = data["subject"]
+		message = data["message"]
+
+		csv_writer = csv.writer(database_csv, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+		csv_writer.writerow([email,subject,message])
+
+
+
+# @app.route('/project.html')
+# def projects():
+# 	return render_template('project.html')
+
+# @app.route('/components.html')
+# def components():
+# 	return render_template('components.html')
+
